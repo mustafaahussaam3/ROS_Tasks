283e0951084892b6afba367c37d98a364e310116
mustafaahussaam3 <mustafahussameldin@gmail.com>
2023-09-01 12:45:44 +0300

    adding launch file

---
+++ urdf_description/build/robot1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mustaf/Documents/ROS Course/Tasks/urdf_description/src/robot1
+# Build directory: /home/mustaf/Documents/ROS Course/Tasks/urdf_description/build/robot1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
