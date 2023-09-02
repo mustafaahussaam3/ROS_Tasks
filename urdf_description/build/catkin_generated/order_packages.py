bc0c1f2492526df0f4fff5cc0ef13c0de1d3932d
mustafaahussaam3 <mustafahussameldin@gmail.com>
2023-09-02 11:42:19 +0300

    removing description file for some bugs

--- urdf_description/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/mustaf/Documents/ROS Course/Tasks/urdf_description/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
--- urdf_description/build/robot1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mustaf/Documents/ROS Course/Tasks/urdf_description/src/robot1
-# Build directory: /home/mustaf/Documents/ROS Course/Tasks/urdf_description/build/robot1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
